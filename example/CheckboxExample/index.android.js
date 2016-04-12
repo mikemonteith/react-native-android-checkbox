ab765c5d112470aaadeaef543676e0b9955e9889
Mike Monteith <mike@mikemonteith.com>
2016-04-12 22:39:34 +0100

    add example app

---
+++ example/CheckboxExample/index.android.js
@@ -0,0 +1,70 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+var Checkbox = require('react-native-android-checkbox');
+
+class CheckboxExample extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: true,
+    };
+
+    this._onValueChange = (value) => {
+      this.setState({value});
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Shake or press menu button for dev menu
+        </Text>
+        <Text>{this.state.value ? 'On' : 'Off'}</Text>
+        <Checkbox
+          value={this.state.value}
+          onValueChange={this._onValueChange}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('CheckboxExample', () => CheckboxExample);
