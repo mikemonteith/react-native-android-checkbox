c9a2b0d016cadf1781dff21d667c350aabdbb2ce
Stuart Lee <updownleftright@gmail.com>
2017-08-08 15:14:48 +0100

    remove override to allow compatibility with RN 0.47
    createJSModules is now not required on Android from RN 0.47.
    
    cannot currently build app because of the `@overide` marker
    
    quickest fix is to just take it out as this maintains backwards compatibility

--- android/src/main/java/com/mikemonteith/reactnativeandroidcheckbox/CheckboxPackage.java
+++ android/src/main/java/com/mikemonteith/reactnativeandroidcheckbox/CheckboxPackage.java
@@ -24,7 +24,6 @@ public class CheckboxPackage implements ReactPackage {
         );
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Arrays.asList();
     }
